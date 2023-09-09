df9c12f96874db712717f8543c1da45882794ae6
Francisco Cuadrado <fr4nx35c0@gmail.com>
2023-09-09 16:50:37 +0200

    Menu desplegable formato movil

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,6 +18,11 @@ export default function NavBar() {
 
     const [isLoading, setIsLoading] = useState(true);
     const [isLoged, setIsLoged] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+      };
 
     const logout = () => {
         signOut(auth);
@@ -74,7 +79,12 @@ export default function NavBar() {
                     <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                                <button 
+                                    onClick={toggleMenu}
+                                    type="button" 
+                                    className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-green-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
+                                    aria-controls="mobile-menu" 
+                                    aria-expanded="false">
                                     <span className="absolute -inset-0.5"></span>
                                     <span className="sr-only">
                                         {t('open_main_menu')}
@@ -124,7 +134,7 @@ export default function NavBar() {
                             </div>
                         </div>
                     </div>
-                    <div className="sm:hidden" id="mobile-menu">
+                    <div className={menuOpen ? "block" : "hidden"} id="mobile-menu">
                         <div className="space-y-1 px-2 pb-3 pt-2">
                             {isLoged
                                 ? loggedRoutes.map((route) => (<Link key={route.id} href={route.href} className="text-white hover:bg-green-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium">{route.name}</Link>))
